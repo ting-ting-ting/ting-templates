333327fdf05054463079abd8233253f36d82be66
TingTing <ting@rytass.com>
2023-03-27 19:06:22 +0800

    global scss config

---
+++ apps/client/pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang='zh-tw'>
+        <Head>
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;500;700;900&family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          {/* noscript for */}
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
