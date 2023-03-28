9a53c000ca5c24ae9a8703e90aaf069d7f18ea75
TingTing <ting@rytass.com>
2023-03-28 18:20:16 +0800

    header layout

--- apps/client/core/ui/Header/index.tsx
+++ apps/client/core/ui/Header/index.tsx
@@ -1,6 +1,44 @@
+import {
+  AppBar,
+  AppBarBrand,
+  AppBarMain,
+  AppBarSupport,
+  Navigation,
+  NavigationItem,
+} from '@mezzanine-ui/react';
+import Image from 'next/image';
+import logo from '@public/images/mezzanine.svg';
+
 const Header: FC = () => {
   return (
-    <div>Header</div>
+    <AppBar>
+      <AppBarBrand>
+        <Image src={logo} alt="" />
+      </AppBarBrand>
+      <AppBarMain>
+        <Navigation>
+          <NavigationItem
+            key="Foundation"
+          >
+            Foundation
+          </NavigationItem>
+          <NavigationItem
+            key="Components"
+            active
+          >
+            Components
+          </NavigationItem>
+          <NavigationItem
+            key="Resources"
+          >
+            Resources
+          </NavigationItem>
+        </Navigation>
+      </AppBarMain>
+      <AppBarSupport>
+        Search
+      </AppBarSupport>
+    </AppBar>
   );
 }
 
--- apps/client/pages/_document.tsx
+++ apps/client/pages/_document.tsx
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang='zh-tw'>
         <Head>
-          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+          <link rel="icon" href="/logo.jpg" type="image/x-icon" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;500;700;900&family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
