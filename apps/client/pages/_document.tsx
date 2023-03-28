fcde1e770387bd44cdac4f6cb170d4d34066c98e
TingTing <ting@rytass.com>
2023-03-28 19:23:18 +0800

    font family config

--- apps/client/pages/_document.tsx
+++ apps/client/pages/_document.tsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
           <link rel="icon" href="/logo.jpg" type="image/x-icon" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;500;700;900&family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;500;700;900&family=Noto+Sans+TC:wght@400;500;700;900&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -4,6 +4,7 @@ function Index() {
   return (
     <div>
       <Typography variant="h1">H1 - Medium</Typography>
+      <Typography variant="h1">我是中文</Typography>
     </div>
   );
 }
