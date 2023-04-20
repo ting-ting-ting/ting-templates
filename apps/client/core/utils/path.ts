a76d623a04bc999a5922e7331b742b94e34bfd63
TingTing <ting@rytass.com>
2023-04-20 19:08:57 +0800

    next paths config

---
+++ apps/client/core/utils/path.ts
@@ -0,0 +1,14 @@
+import { GenerateNextPath as NextPath } from './generateNextPath';
+
+export function nextJsDynamicPath<T extends string>(nextPath: T, param: string) {
+  return nextPath.replace(/\[\S+\]/, param);
+}
+
+export function getNextJsDynamicPathParamName<T extends string>(nextPath: T) {
+  return nextPath.match(/\[(.+?)\]$/)?.[1];
+}
+
+export { GenerateNextPath as NextPath } from './generateNextPath';
+
+export const dynamicPath = nextJsDynamicPath<NextPath>;
+export const getPathParamName = getNextJsDynamicPathParamName<NextPath>;
---
+++ apps/client/pages/calendar.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@mezzanine-ui/react';
+
+function Calendar() {
+  return (
+    <div>
+      <Typography variant="h1">Calendar</Typography>
+    </div>
+  );
+}
+
+export default Calendar;
